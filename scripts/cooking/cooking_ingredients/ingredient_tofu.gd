3657f0b8bc6d43d3bbaba737a7228a4cf2929f04
Piyan0 <piyanapriyanto.0@gmail.com>
2025-10-06 22:08:00 +0700

    fix: fix return callable of 'ingredients_count()' method.

--- scripts/cooking/cooking_ingredients/ingredient_tofu.gd
+++ scripts/cooking/cooking_ingredients/ingredient_tofu.gd
@@ -5,4 +5,6 @@ func _id() -> int:
   return DB.Ingredient.TOFU
   
 func _is_can_place(in_plate: Array[CookingIngredient]) -> bool:
-  return true
+  var utils: CookingUtils= CookingUtils.new(in_plate)
+
+  return utils.ingredients_count(DB.Ingredient.RICE_ROLL) < 1
