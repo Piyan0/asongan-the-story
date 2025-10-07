14c2bef9460c4318beb541f3913221b254941388
Piyan0 <piyanapriyanto.0@gmail.com>
2025-10-07 21:38:06 +0700

    style add contecxt comment on EventsID.

--- scripts/events_id.gd
+++ scripts/events_id.gd
@@ -2,6 +2,15 @@ extends Node
 class_name EventsID
 
 
+# Note, when asigning enum in editor, the value would always the same,
+#for example, if i set in editor the value MAIN_ROAD_001 ( currently 0 ), the editor will always have value 0,
+#even tough we change MAIN_ROAD_001. Also, I need to restart the editor for it to update the enum if the enum is changed.
+
+#so I can't add new value in the middle of value, for example I have MAIN_ROAD_001, MAIN_ROAD_002, I cannot
+#add MAIN_ROAD_003 in the middle of it, since it will break the compatibilty, therefore, I add it below MAIN_ROAD_002.
+
+#and note that I add =50 at SHOP_001, so when I add new entry in MAIN_ROAD_, the starting of SHOP_ won't change.
+#just make sure that the event in each room/scene won't exceed 50, if it is, consider changing to bigger gap, i.e 100.
 enum ID{
   MAIN_ROAD_001=0,
   MAIN_ROAD_002,
