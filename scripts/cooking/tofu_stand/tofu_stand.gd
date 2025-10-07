4e4dbd877d3c28a6b8ddf08fb353e674096b65e1
Piyan0 <piyanapriyanto.0@gmail.com>
2025-10-07 09:16:40 +0700

    add: add INVENTORY_MAXED translation.

--- scripts/cooking/tofu_stand/tofu_stand.gd
+++ scripts/cooking/tofu_stand/tofu_stand.gd
@@ -13,7 +13,7 @@ var current_tofu: int= 0
 var ingredient_used_count: Dictionary
 
 func _ready() -> void:
-  TranslationServer.set_locale('id-ID')
+  #TranslationServer.set_locale('id-ID')
   ingredient_ui= {
     DB.Ingredient.TOFU: %tofu,
     DB.Ingredient.RICE_ROLL: %rice_roll,
@@ -141,7 +141,7 @@ func close():
   pass
 
 func on_unable_to_place() -> void:
-  OverlayManager.show_alert('Inventory maxed.')
+  OverlayManager.show_alert('INVENTORY_MAXED')
   
 func _process(delta: float) -> void:
   mouse.position= get_global_mouse_position()
