586fa5e00817f3fa998b684a8cef43f779c43407
Piyan0 <piyanapriyanto.0@gmail.com>
2025-11-08 22:22:51 +0700

    Fix RESET hint when opening stand.

--- scripts/events/main_road/005.gd
+++ scripts/events/main_road/005.gd
@@ -2,9 +2,12 @@ extends Node
 
 #events start with _+number, the d, g is injected in EventArea.
 func _1(d: Callable, g: GameEvent):
+  ControlHint.instance.save_hint('c')
+  ControlHint.instance.set_hint('c', 'RESET')
   await OverlayManager.show_overlay(
     OverlayManager.Overlay.COFFE_STAND
   )
+  ControlHint.instance.set_hint('c', 'prev')
 # This will be called when player leave the area, if 'trigger_by_enter is set to true.
 func _1_after(d: EventUniqueData, g: GameEvent):
   pass
